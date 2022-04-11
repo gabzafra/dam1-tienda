e06a9b95b283ebd6bde67b05abf7f90e05a8602b
Gabriel Zafra <gab.zafra@gmail.com>
2022-04-11 10:57:52 +0200

    bd con solo tablas

--- src/dam1/prog/tiendav2/MockDB.java
+++ src/dam1/prog/tiendav2/MockDB.java
@@ -2,26 +2,17 @@ package dam1.prog.tiendav2;
 
 public class MockDB {
     private User[] usersTable;
-    private Order[] orderTable;
+    private Order[] ordersTable;
+    private Shoe[] productsTable;
+    private int[][] userOrdersTable;
+    private int[][] ordersProductsTable;
 
-  public MockDB(User[] usersTable, Order[] orderTable) {
+  public MockDB(User[] usersTable, Order[] ordersTable, Shoe[] productsTable,
+      int[][] userOrdersTable, int[][] ordersProductsTable) {
     this.usersTable = usersTable;
-    this.orderTable = orderTable;
-  }
-
-  public User[] getUsersTable() {
-    return usersTable;
-  }
-
-  public void setUsersTable(User[] usersTable) {
-    this.usersTable = usersTable;
-  }
-
-  public Order[] getOrderTable() {
-    return orderTable;
-  }
-
-  public void setOrderTable(Order[] orderTable) {
-    this.orderTable = orderTable;
+    this.ordersTable = ordersTable;
+    this.productsTable = productsTable;
+    this.userOrdersTable = userOrdersTable;
+    this.ordersProductsTable = ordersProductsTable;
   }
 }

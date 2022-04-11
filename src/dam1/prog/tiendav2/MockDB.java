eed1422290b720801c5a28ba7acf4773dd1cd488
Gabriel Zafra <gab.zafra@gmail.com>
2022-04-11 10:30:37 +0200

    bean para la bd

--- src/dam1/prog/tiendav2/MockDB.java
+++ src/dam1/prog/tiendav2/MockDB.java
@@ -1,5 +1,27 @@
 package dam1.prog.tiendav2;
 
 public class MockDB {
-//pensar si separo la API de la bd
+    private User[] usersTable;
+    private Order[] orderTable;
+
+  public MockDB(User[] usersTable, Order[] orderTable) {
+    this.usersTable = usersTable;
+    this.orderTable = orderTable;
+  }
+
+  public User[] getUsersTable() {
+    return usersTable;
+  }
+
+  public void setUsersTable(User[] usersTable) {
+    this.usersTable = usersTable;
+  }
+
+  public Order[] getOrderTable() {
+    return orderTable;
+  }
+
+  public void setOrderTable(Order[] orderTable) {
+    this.orderTable = orderTable;
+  }
 }

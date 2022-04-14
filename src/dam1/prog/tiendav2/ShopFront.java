a3a14edad5dc69faae46ec511b22f3f59d8b89a4
Gabriel Zafra <gab.zafra@gmail.com>
2022-04-14 11:52:47 +0200

    funcion auxiliar para pintar menu y funcion para entrada usuario

--- src/dam1/prog/tiendav2/ShopFront.java
+++ src/dam1/prog/tiendav2/ShopFront.java
@@ -3,6 +3,7 @@ package dam1.prog.tiendav2;
 import java.util.Scanner;
 
 public class ShopFront {
+
   //Colores
   private static final String COLOR_WHITE = "\u001B[0m";
   private static final String COLOR_RED = "\u001B[31m";
@@ -13,13 +14,40 @@ public class ShopFront {
     Menu currentMenu = Menu.MENU_1;
     int selectedOption;
     do {
-      System.out.println(currentMenu.getTitle());
-      for (MenuItem option: currentMenu.getOptionList()
-      ) {
-        System.out.println(option.getOptionNumber() + ". " + option.getOptionLabel());
+      selectedOption = getOptionFromUser(input, currentMenu);
+    } while (selectedOption != 0);
+  }
+
+  private static void pintarMenu(Menu menu){
+    System.out.println(menu.getTitle());
+    System.out.println("---------------------------------------");
+    for (MenuItem option : menu.getOptionList()
+    ) {
+      System.out.println(option.getOptionNumber() + ". " + option.getOptionLabel());
+    }
+    System.out.println("---------------------------------------");
+  }
+
+  private static int getOptionFromUser(Scanner consoleInput, Menu menu) {
+    int userInput = -1;
+    boolean isAnInt;
+    while (true) {
+      pintarMenu(menu);
+      isAnInt = true;
+      System.out.println("Por favor elija una opción del menú:");
+      try {
+        userInput = Integer.parseInt(consoleInput.nextLine());
+      } catch (NumberFormatException e) {
+        isAnInt = false;
+        System.out.println(COLOR_RED + "Debe escribir un número!" + COLOR_WHITE);
       }
-      System.out.println("Por favor elija una opción");
-      selectedOption = input.nextInt();
-    }while (selectedOption != 0);
+      if (isAnInt) {
+        for (MenuItem opcion : menu.getOptionList()) {
+          if (opcion.getOptionNumber() == userInput) {
+            return userInput;
+          }
+        }
+      }
+    }
   }
 }

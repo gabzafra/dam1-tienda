d5c7b74e353aff8e9ff8db493df0abfa7f68cfc8
Gabriel Zafra <gab.zafra@gmail.com>
2022-04-14 11:14:01 +0200

    creando estructuras para guardar los datos de los menus

--- src/dam1/prog/tiendav2/ShopFront.java
+++ src/dam1/prog/tiendav2/ShopFront.java
@@ -1,7 +1,15 @@
 package dam1.prog.tiendav2;
 
+import java.util.Scanner;
+
 public class ShopFront {
+  //Colores
+  private static final String COLOR_WHITE = "\u001B[0m";
+  private static final String COLOR_RED = "\u001B[31m";
+  private static final String COLOR_GREEN = "\u001B[32m";
 
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
   }
 }

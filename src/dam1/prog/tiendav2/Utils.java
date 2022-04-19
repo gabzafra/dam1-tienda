a854317f803e6f732ac076debc83d927a77c92c0
Gabriel Zafra <gab.zafra@gmail.com>
2022-04-19 18:04:34 +0200

    funcion para comprobar si un string es un int

--- src/dam1/prog/tiendav2/Utils.java
+++ src/dam1/prog/tiendav2/Utils.java
@@ -2,4 +2,19 @@ package dam1.prog.tiendav2;
 
 public class Utils {
 
+  /**
+   *
+   * Dada una cadena comprueba si puede convertirse en entero
+   *
+   * @param number cadena con un entero
+   * @return true si puede convertirse, false si no
+   */
+  static boolean isIntString(String number) {
+    try {
+      Integer.parseInt(number);
+    } catch (NumberFormatException e) {
+      return false;
+    }
+    return true;
+  }
 }
